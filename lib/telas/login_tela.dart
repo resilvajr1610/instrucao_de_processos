fe6dd429f0d8afd1a82ba2a9377d27ced88261a1
Reginaldo <resilvajr@hotmail.com>
2024-02-14 12:49:16 -0300

    cores iniciais

--- lib/telas/login_tela.dart
+++ lib/telas/login_tela.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instrucao_de_processos/utilidades/paleta_cores.dart';
 
 class LoginTela extends StatefulWidget {
   const LoginTela({super.key});
@@ -10,6 +11,25 @@ class LoginTela extends StatefulWidget {
 class _LoginTelaState extends State<LoginTela> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerRight,
+            end: Alignment.centerLeft,
+            colors: PaletaCores().degrade_azul, // Define your gradient colors here
+          ),
+        ),
+        child: Center(
+          child: Text(
+            'Hello, Flutter!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24.0,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utilidades/paleta_cores.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class PaletaCores{
+  Color azul_escuro_degrade = Color(0xff0A0879);
+  Color azul_claro_degrade = Color(0xff2622F1);
+  List<Color> degrade_azul = [Color(0xff2622F1),Color(0xff0A0879)];
+}
