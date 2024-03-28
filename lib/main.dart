1b5e5072f0935f1b219b8452961cff52a6a6dc27
Reginaldo <resilvajr@hotmail.com>
2024-03-28 20:33:05 -0300

    ajustes do js flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instrucao_de_processos/telas/login_tela.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:instrucao_de_processos/telas/teste.dart';
-import 'package:instrucao_de_processos/telas/usuarios_tela.dart';
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
-  kIsWeb? await Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: "AIzaSyDMZEqs-YW1yQ9q2EzT6Mf6STnSnQ9Du1I",
-      appId: "1:349119442488:web:61c64129c8e1e1d4f400cd",
-      messagingSenderId:"349119442488",
-      projectId: "instrucao-de-trabalho",
-      storageBucket: "instrucao-de-trabalho.appspot.com",
-    ),
-  ):await Firebase.initializeApp();
+        apiKey: "AIzaSyDMZEqs-YW1yQ9q2EzT6Mf6STnSnQ9Du1I",
+        authDomain: "instrucao-de-trabalho.firebaseapp.com",
+        projectId: "instrucao-de-trabalho",
+        storageBucket: "instrucao-de-trabalho.appspot.com",
+        databaseURL: "https://instrucao-de-trabalho.firebaseio.com",
+        messagingSenderId: "349119442488",
+        appId: "1:349119442488:web:61c64129c8e1e1d4f400cd",
+        measurementId: "G-BFCT2REF57"
+    )
+  );
   runApp(const MyApp());
 }
 
